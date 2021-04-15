ce0d0904873b6143662a34a0a5a0e51744812500
Miguel Ángel Rodríguez Dalí <migueadali@gmail.com>
2021-04-15 16:38:54 +0200

    Unimos las tablas necesarias para la consulta 1

--- SQL_Apoyo/Refuerzo_20210415.sql
+++ SQL_Apoyo/Refuerzo_20210415.sql
@@ -1,1 +1,11 @@
-a
+/*
+ * En la base de datos de jardinería
+ */
+ 
+--DECIR EL PAIS DONDE SE HA GASTADO MAS  
+/* Con esto de aquí abajo lo que hacemos es el unir las tablas */
+SELECT *
+FROM CLIENTES C, PEDIDOS P, DETALLEPEDIDOS DP
+WHERE C.CODIGOCLIENTE = P.CODIGOCLIENTE
+AND P.CODIGOPEDIDO = DP.CODIGOPEDIDO
+;
