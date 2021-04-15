9b9a0ec3cf2c64d1cc1292148302044aaba10981
Miguel Ángel Rodríguez Dalí <migueadali@gmail.com>
2021-04-15 09:05:40 +0200

    Primer ejercicio del boletín "fácil" hecho
    Aquí tengo subido y comentado el primer ejercicios con apuntes que he hecho de las cosas dichas por Antonio. Teniendo la primera función y su prueba, con las salidas para los errores correspondientes.

--- Clases_SQL/20210415_PLSQL.sql
+++ Clases_SQL/20210415_PLSQL.sql
@@ -1,1 +1,33 @@
 --Aquí iré poniendo las cosas del día.
+--De primeras hacemos los ejercicios del siguiente boletín: PLSQL-EjerciciosFaciles.pdf
+--La tabla usada es la de empleados y departamentos.
+
+/* Haz una función llamada DevolverCodDept que reciba el nombre de un departamento y devuelva su código. */
+CREATE OR REPLACE FUNCTION DevolverCodDept(nom in dept.dname%type) 
+return dept.deptno%type
+is
+
+  v_cod dept.deptno%type := 0;
+  
+begin
+
+  select deptno
+    into v_cod
+  from dept
+  where upper(dname) = upper(nom);
+  
+  RETURN v_cod;
+
+exception
+when no_data_found then
+  return -1;            --Con esto gestionamos mejor el error y saca un -1 cuando no encuentra datos.
+when others then       --Y en este se controlan el resto de errores con el mensaje error.
+RETURN 'ERROR';
+
+end;
+/
+show errors;
+/
+--Con esto tendríamos creada la función correspondiente.
+select DevolverCodDept('SaLeS') from dual;
+--Y esta línea del select es una prueba de que la función está hecha correctamente.
